b9e880dc202427da0899416e1b2e941b37fd76de
Alper Aricay <>
2024-05-23 19:37:48 +0300

    Implemented slot object model

---
+++ Assets/Game/Scripts/Gameplay/SlotModule/Model/SlotObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.SlotModule.Model
+{
+    [CreateAssetMenu(fileName = "Slot_Object", menuName = "Create Slot Object", order = 0)]
+    public class SlotObject : ScriptableObject
+    {
+        public SlotObjectType Type;
+        public int PrizeValue;
+        public Sprite BlurredSprite;
+        public Sprite OriginalSprite;
+        
+        public enum SlotObjectType
+        {
+            A = 0,
+            Bonus,
+            Seven,
+            Wild,
+            Jackpot
+        }
+    }
+}
